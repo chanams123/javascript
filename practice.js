fad8a093eb10438a7cbe0b1db20c0d49de972bc1
chanams123 <chanamspielman@gmail.com>
2026-06-17 13:29:33 -0400

    Your message

--- practice.js
+++ practice.js
@@ -11,17 +11,19 @@ searchBtn.addEventListener('click', async function() {
         const data = await response.json();
         if (data.Search) {
             results.innerHTML = "";
+            const movies = data.Search;
+            movies.sort((a, b) => a.Title.localeCompare(b.Title));
             data.Search.forEach(function(movie) {
                 results.innerHTML +=
                 `<div>
                     <h3>${movie.Title}</h3>
                     <p>${movie.Year}</p>
-                </div>`;
-            });
-        } else {
+                    <img>${movie.img}
+                    </div>`;
+                });
+            } else {
             results.innerHTML = "<p>No results found.</p>";
         }
     }
-const movies = data.Search;
-movies.sort((a, b) => a.Title.localCompare(b.Title));
-});
+    
+    });
